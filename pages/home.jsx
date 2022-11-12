9b1f3f4e0dc881d2f4329aa7eef74a60a72b53bf
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-13 10:00:22 +1100

    refacoring

---
+++ pages/home.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const Home = ({}) => {
+  return <div>Home</div>;
+};
+
+export default Home;
