ce166ef21f6a18fa98d755d0d234be52bb637238
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-15 09:47:10 +1100

    setup of axios

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import axios from "axios";
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  const handleLogin = async () => {
+    console.log("CLICKED");
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login",
+        {
+          name: "Pieter-Jan",
+          password: " 123987",
+        },
+        {
+          headers: {
+            contentType: "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h5>Login</h5>
+      <button onClick={handleLogin}>SEND</button>
+    </>
+  );
 };
 
 export default LoginPage;
