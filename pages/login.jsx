4abbccb6876b0bd7e82da1c4f6f64aa09f7e24ff
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-24 09:19:29 +1100

    view enabled for passwords

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,29 +4,28 @@ import axios from "axios";
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [viewEnabled, setViewEnabled] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    console.log("CLICKED");
-    console.log("EMAIL:", email);
-    console.log("Password:", password);
-    // try {
-    //   const response = await axios.post(
-    //     "http://localhost:3000/login",
-    //     {
-    //       email: "Pieter-Jan",
-    //       password: " 123987",
-    //     },
-    //     {
-    //       headers: {
-    //         contentType: "application/json",
-    //       },
-    //     }
-    //   );
-    //   console.log(response);
-    // } catch (error) {
-    //   console.error(error);
-    // }
+    console.log("viewenabled", viewEnabled);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login",
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            contentType: "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -44,10 +43,14 @@ const LoginPage = () => {
       <div>
         <label htmlFor="password">Password</label>
         <input
-          type="text"
+          type={viewEnabled ? "text" : "password"}
           name="password"
           id="password"
           onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="checkbox"
+          onChange={(e) => setViewEnabled(e.target.checked)}
         />
       </div>
       <button onClick={handleLogin}>SEND</button>
