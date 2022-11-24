c6314f484f49ff13738d8f1bf43bd067357168e5
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-25 09:03:35 +1100

    handle state for sign-up form

--- pages/sign-up.jsx
+++ pages/sign-up.jsx
@@ -1,54 +1,123 @@
 import { useState, useEffect } from "react";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [streetName, setStreetName] = useState("");
+  const [streetNumber, setStreetNumber] = useState("");
+  const [unitNumber, setUnitNumber] = useState("");
+  const [suburb, setSuburb] = useState("");
+  const [postNumber, setPostNumber] = useState("");
+  const [state, setState] = useState("");
+
+  const handleSubmit = () => {};
   return (
     <>
       <h5>SignUp</h5>
       <form>
         <div>
           <label htmlFor="email">email</label>
-          <input type="text" name="email" id="email" />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">password</label>
-          <input type="text" name="password" id="password" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">re-enter password</label>
-          <input type="text" name="rePassword" id="rePassword" />
+          <input
+            type="text"
+            name="rePassword"
+            id="rePassword"
+            onChange={(e) => setRePassword(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" id="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" id="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="streetName">Street Name</label>
-          <input type="text" name="streetName" id="streetName" />
+          <input
+            type="text"
+            name="streetName"
+            id="streetName"
+            onChange={(e) => setStreetName(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="streetNumber">Street Number</label>
-          <input type="text" name="streetNumber" id="streetNumber" />
+          <input
+            type="text"
+            name="streetNumber"
+            id="streetNumber"
+            onChange={(e) => setStreetNumber(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="unitNumber">Unit Number</label>
-          <input type="text" name="unitNumber" id="unitNumber" />
+          <input
+            type="text"
+            name="unitNumber"
+            id="unitNumber"
+            onChange={(e) => setUnitNumber(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="suburb">Suburb</label>
-          <input type="text" name="suburb" id="suburb" />
+          <input
+            type="text"
+            name="suburb"
+            id="suburb"
+            onChange={(e) => setSuburb(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="postNumber">postNumber</label>
-          <input type="text" name="postNumber" id="postNumber" />
+          <input
+            type="text"
+            name="postNumber"
+            id="postNumber"
+            onChange={(e) => setPostNumber(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="state">State</label>
-          <input type="text" name="state" id="state" />
+          <input
+            type="text"
+            name="state"
+            id="state"
+            onChange={(e) => setState(e.target.value)}
+          />
         </div>
+        <button onClick={handleSubmit}>SUBMIT</button>
       </form>
     </>
   );
